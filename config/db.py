09704523446546e151a9e37bd475f82413258ccf
Vivekkakadiya22 <vivekkakadiya84197@gmail.com>
2025-11-06 11:36:49 +0530

    Update MongoDB URI to placeholder
    Replaced MongoDB connection string with a placeholder.

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://kvivek123:vivek123@mongoyt.bi1wcd7.mongodb.net/"
+MONGO_URI = "ENTER YOUR MONGODB URL HERE"
 conn = MongoClient(MONGO_URI)
 
